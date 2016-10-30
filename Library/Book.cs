0c8feb45a2b27bc3336e04ba6c748be9debea65c
Berger <zygintasb@gmail.com>
2016-10-30 16:16:13 +0200

    Add Book class

---
+++ Library/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+	class Book
+	{
+		public string ISBN { get; set; }
+		public string Title { get; set; }
+		public string Author { get; set; }
+		public short Year { get; set; }
+		public int NumPages { get; set; }
+
+		public Book(string ISBN, string title, string author, short year, int numPages)
+		{
+			this.ISBN = ISBN;
+			this.Title = title;
+			this.Author = author;
+			this.Year = year;
+			this.NumPages = numPages;
+		}
+	}
+}
