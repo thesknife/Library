1879c87174666d29ad49c41f921f24f951308e09
Berger <zygintasb@gmail.com>
2016-10-30 18:22:18 +0200

    Add Reader class

---
+++ Library/Reader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+	class Reader
+	{
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public int ID { get; set; }
+		//change book to issue?
+		public List<Book> takenBooks = new List<Book>();
+		//context info, not required for fully functioning program
+		public int AK { get; set; }
+		public DateTime DateOfBirth { get; set; }
+		public string Adress { get; set; }
+
+		public Reader(string name, string surname)
+		{
+			this.Name = name;
+			this.Surname = surname;
+			this.ID = GetHashCode();
+		}
+
+	}
+}
