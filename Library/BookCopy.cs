ace0ac8a1a3c6bdd7473f0320131085948cc51b4
Berger <zygintasb@gmail.com>
2016-10-30 20:18:52 +0200

    Add BookCopy class

---
+++ Library/BookCopy.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+	class BookCopy
+	{
+		public Book Book { get; set; }
+		public Reader Reader { get; set; }
+		public DateTime? DateTaken { get; set; }
+		public DateTime? DateReturn { get; set; }
+		public int ID { get; set; }
+		public bool Taken { get; set; }
+
+		public BookCopy(Book book)
+		{
+			Book = book;
+			ID = GetHashCode();
+			Taken = false;
+		}
+
+		public void BookTaken(Reader reader)
+		{
+			Taken = true;
+			Reader = reader;
+			DateTaken = DateTime.Today;
+			DateReturn = DateTime.Today.AddDays(30);
+		}
+
+		public void BookReturned()
+		{
+			Taken = false;
+			Reader = null;
+			DateTaken = null;
+			DateReturn = null;
+		}
+	}
+}
